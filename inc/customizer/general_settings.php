e5c3313c5dade9b9145c64971943ebab2c224cdb
Ashraf Uddin <ashraf@Ashrafs-MacBook-Pro.local>
2021-12-02 10:50:51 +0600

    fixed sidebar issue

--- inc/customizer/general_settings.php
+++ inc/customizer/general_settings.php
@@ -1,6 +1,6 @@
 <?php
-Kirki::add_section( 'general_settings', array(
-	'title'          => esc_html__( 'General Option', 'finest-mini-cart' ),
+Kirki::add_section( 'doc_sidebar', array(
+	'title'          => esc_html__( 'Sidebar Option', 'finest-docs' ),
 	'panel'          => 'docs_panel',
 	'priority'       => 160,
 ) );
@@ -11,17 +11,30 @@ Kirki::add_field( 'docs_panel', [
 	'type'        => 'select',
 	'settings'    => 'sidebar_settings_area',
 	'label'       => esc_html__( 'Sidebar Left Or Right', 'finest-docs' ),
-	'section'     => 'general_settings',
+	'section'     => 'doc_sidebar',
 	'default'     => 'left',
 	'priority'    => 10,
 	'multiple'    => 1,
 	'choices'     => [
 		'left' => esc_html__( 'Left', 'finest-docs' ),
 		'right' => esc_html__( 'Right', 'finest-docs' ),
+		'none' => esc_html__( 'None', 'finest-docs' ),
 		
 	],
 	
 ] );
 
 
-
+Kirki::add_field( 'docs_panel', [
+	'type'        => 'radio-image',
+	'settings'    => 'sidebAR_LAYOUT',
+	'label'       => esc_html__( 'Radio Control (Image)', 'kirki' ),
+	'section'     => 'doc_sidebar',
+	'default'     => 'left',
+	'priority'    => 10,
+	'choices'     => [
+		'left'   => 'http://finestwp.local/wp-content/plugins/betterdocs/admin/assets/img/docs-layout-1.png',
+		'center' => get_template_directory_uri() . '/assets/images/center.png',
+		'right'  => get_template_directory_uri() . '/assets/images/right.png',
+	],
+] );
--- templates/single-docs.php
+++ templates/single-docs.php
@@ -5,8 +5,10 @@
 <?php while ( have_posts() ) {
         the_post(); ?>
         <div class="finest-single-wrap">
-            <?php if ( 'left' == $sidebar ): ?>
-            <?php finest_get_template_part( 'finest-docs', 'sidebar' ); ?>
+            <?php if ( 'left' == $sidebar && 'none' != $sidebar ): ?>
+            <?php finest_get_template_part( 'finest-docs', 'sidebar' ); ?> 
+            <?php endif; ?>
+
             <div class="finest-single-content">
                 <?php finest_breadcrumbs(); ?>
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemscope itemscope >
@@ -15,7 +17,7 @@
                     </header>
                     <div class="entry-content" itemprop="articleBody">
 
-                        <div class="autoc" data-stopat='h6' data-offset='1'></div>
+                        <div class="autoc" data-stopat='h2' data-offset='1'></div>
                         <?php
                             the_content( sprintf(
                                 /* translators: %s: Name of current post. */
@@ -23,17 +25,7 @@
                                 the_title( '<span class="screen-reader-text">"', '"</span>', false )
                             ) );
 
-                        
-                        $children = wp_list_pages( 'title_li=&order=menu_order&child_of=' . $post->ID . '&echo=0&post_type=' . $post->post_type );
-
-                        if ( $children ) {
-                            echo '<div class="finest-article-child well">';
-                            echo '<h3>' . __( 'Articles', 'finest-docs' ) . '</h3>';
-                            echo '<ul>';
-                            echo $children;
-                            echo '</ul>';
-                            echo '</div>';
-                        }           
+                     
                         ?>
                     </div>
                     <?php if ( comments_open() || get_comments_number() ) { ?>
@@ -43,44 +35,8 @@
                     <?php } ?>
                 </article><!-- #post-## -->
             </div><!-- .finest-single-content -->
-            <?php else: ?>
-                <div class="finest-single-content">
-                <?php finest_breadcrumbs(); ?>
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemscope itemscope >
-                    <header class="entry-header">
-                            <?php the_title( '<h1 class="entry-title" itemprop="headline">', '</h1>' ); ?>
-                    </header>
-                    <div class="entry-content" itemprop="articleBody">
-
-                        <div class="autoc" data-stopat='h6' data-offset='1'></div>
-                        <?php
-                            the_content( sprintf(
-                                /* translators: %s: Name of current post. */
-                                wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'finest-docs' ), [ 'span' => [ 'class' => [] ] ] ),
-                                the_title( '<span class="screen-reader-text">"', '"</span>', false )
-                            ) );
-
-                        
-                        $children = wp_list_pages( 'title_li=&order=menu_order&child_of=' . $post->ID . '&echo=0&post_type=' . $post->post_type );
-
-                        if ( $children ) {
-                            echo '<div class="finest-article-child well">';
-                            echo '<h3>' . __( 'Articles', 'finest-docs' ) . '</h3>';
-                            echo '<ul>';
-                            echo $children;
-                            echo '</ul>';
-                            echo '</div>';
-                        }           
-                        ?>
-                    </div>
-                    <?php if ( comments_open() || get_comments_number() ) { ?>
-                            <div class="finest-comments-wrap">
-                                <?php comments_template(); ?>
-                            </div>
-                    <?php } ?>
-                </article><!-- #post-## -->
-            </div><!-- .finest-single-content -->
-            <?php finest_get_template_part( 'finest-docs', 'sidebar' ); ?>
+            <?php if ( 'right' == $sidebar && 'none' != $sidebar ): ?>
+            <?php finest_get_template_part( 'finest-docs', 'sidebar' ); ?> 
             <?php endif; ?>
         </div><!-- .finest-single-wrap -->
 
